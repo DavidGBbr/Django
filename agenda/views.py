cf364313d17969715e008ba3354b8ee29e6fb84c
davidGBbr <davidgomesbrigido@gmail.com>
2024-05-09 13:55:59 -0300

    django template

--- agenda/views.py
+++ agenda/views.py
@@ -1,17 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from agenda.models import events
+from django.template import loader
 # Create your views here.
 def index(request):
   return HttpResponse("Olá mundo!")
 
 def show_event(request):
-  event = events[1]
-  return HttpResponse(f""" 
-  <html>
-    <h1>Evento: {event.nome}</h1>                    
-    <p>Evento: {event.categoria}</p>                    
-    <p>Evento: {event.local}</p>                    
-    <p>Evento: {event.link}</p>                    
-  </html>
-""")
+  event = events[0]
+  # template = loader.get_template("agenda/show_event.html")
+  # rendered_template = template.render(context={"event": event}, request=request)
+  # return HttpResponse(rendered_template)
+  return render(request=request, context={"event": event}, template_name="agenda/show_event.html")
