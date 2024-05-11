1097b73206809f649f8c76769b0b5c623e204753
davidGBbr <davidgomesbrigido@gmail.com>
2024-05-11 17:11:21 -0300

    Django admin

--- agenda/models.py
+++ agenda/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Category(models.Model):
   name = models.CharField(max_length=255)
 
+  def __str__(self):
+    return f'{self.name} <{self.id}>'
+
 class Event(models.Model):
   name = models.CharField(max_length=255)
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
   local = models.CharField(max_length=255, blank=True)
   link = models.CharField(max_length=255, blank=True)
+
+  def __str__(self):
+      return f'{self.name} <{self.id}>'
